2ed48a730ed80fca2b15c2ca95fa7032fb30fdf0
EstherFaBer <estherfaundez@gmail.com>
2026-07-01 22:27:14 +0100

    background

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ const CANVAS_WIDTH = 416;
 const CANVAS_HEIGHT = 416;
 
 
-//canvas set up
+//canvas setup
 function createCanvas(): HTMLCanvasElement {
   const canvas = document.createElement('canvas');
   canvas.width = CANVAS_WIDTH;
@@ -16,3 +16,35 @@ function createCanvas(): HTMLCanvasElement {
 
   return canvas;
 }
+
+//Render
+function render(ctx: CanvasRenderingContext2D): void {
+  //background in black
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  //draw a green rectangle
+  ctx.fillStyle = '#4caf50';
+  ctx.fillRect(192, 192, 32, 32);
+
+  //text
+  ctx.fillStyle = '#ffffff';
+  ctx.font = '20px monospace';
+  ctx.textAlign = 'center';
+  ctx.fillText('Super Tank World', CANVAS_WIDTH / 2, 50);
+
+}
+
+//Init = page load
+function init(): void {
+  const canvas = createCanvas();
+
+  const ctx = canvas.getContext('2d')!;
+  render(ctx);
+
+  console.log('Super Tank 1990 initialized.');
+  console.log('Canvas: ${CANVAS_WIDTHx${CANVAS_HEIGHT}');
+}
+
+
+window.addEventListener('DOMContentLoaded', init);
